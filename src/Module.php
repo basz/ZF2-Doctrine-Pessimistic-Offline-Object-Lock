a356cc9e5b1d93f401bab6636879c07c050fb11f
Bas Kamer <bas@bushbaby.nl>
2015-06-01 17:37:44 +0200

    Add Module.php

---
+++ src/Module.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace HF\POOL;
+
+use Zend\ModuleManager\Feature;
+
+class Module implements
+    Feature\ConfigProviderInterface,
+    Feature\DependencyIndicatorInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getModuleDependencies()
+    {
+        return ['DoctrineORMModule'];
+    }
+}
